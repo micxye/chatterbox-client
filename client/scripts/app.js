3f2affb07740fcd79a7b5b4abec3c96289f53ba1
micxye <michaelyeahh@gmail.com>
2018-03-17 14:01:58 -0700

    finish all but last test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,14 +5,30 @@ var app = {
 
 
 app.init = function() {
-  
+  // $('body').on('click', function() {
+  //   this.handleUsernameClick();
+  //   console.log(this);
+  // });
+  $('#send').submit(function(event) {
+    console.log('submitted');
+    var message = $('#message').val();
+    app.handleSubmit(message); //expects message in string form
+    //clear textbox
+    event.preventDefault();
+    // var form = document.getElementById('#message').reset();
+  })
+  // $('.submit').on('click', function(e) {
+  //   app.handleSubmit($('#message'));
+  //   console.log('clicked');
+  // });
+  // console.log('run init');
 };
 
 app.send = function(message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    '*': {'read': true},
-    'message': {'read': true, 'write': false},
+    // '*': {'read': true},
+    // 'message': {'read': true, 'write': false},
     url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
@@ -40,329 +56,47 @@ app.fetch = function() {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to get message', data);
     }
-  }); 
+  });
 }
 
 app.clearMessages = function() {
-  $('#chats').html('');
+  $('#chats').empty();
 }
 
 app.renderMessage = function(message) {
-  var $message = $(`<p class=message>${message.username}: ${message.text}</p>`);
-  $message.on('click', this.handleUserNameClick());
+  var $message = $(`<div class="username">${message.username}: ${message.text}</div>`);
+  $message.on('click', function() {
+    app.friends.push(message.username);
+    app.handleUsernameClick();
+  })
   $message.appendTo('#chats');
-  $('')
-  
+
+  // $('#chats').prepend(`<div class="message" onclick="app.handleUserNameClick()"> ${message.username} </div>`)
+
+  //onclick="app.handleUsernameClick()"
 }
 
 app.renderRoom = function(roomName) {
   $('#roomSelect').append(`<div>${roomName}</div>`);
 }
 
-app.handleUserNameClick = function() {
-  this.friends.push(message.username);
-  console.log('CLICK');
+app.handleUsernameClick = function() {
+  console.log('success');
 }
 
-app.handleSubmit = function() {
+app.handleSubmit = function(message) {
+  var messageObject = {
+    username: 'cameron',
+    text: message,
+    roomname: 'lobby'
+  }
+  app.renderMessage(messageObject);
+
+
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  app.init();
+})
